bd5ae9b49c655691c8069fa4156ea53abcac2034
dc <dcmlcrazy@gmail.com>
2017-09-25 23:18:05 -0400

    Adds command line script

--- script.py
+++ script.py
@@ -1,9 +1,10 @@
 import sys
+import json
 from reddit_client import RedditClient
 from language_processor import classify_posts
 
 
-def display_list(subreddit):
+def display_list(subreddit, food_type):
     '''
     Method invoked from the command line, receives name of a subreddit and name
     of a food_type classification, will invoke the get_subreddit, classify_posts
@@ -12,19 +13,25 @@ def display_list(subreddit):
     '''
     client = RedditClient(subreddit)
     subreddit = client.get_subreddit()
-    try:
-        posts = subreddit['data']['children']
 
-        classified_posts = classify_posts(posts)
+    posts = subreddit.get('data').get('children')
 
-        for post in classified_posts:
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!')
-            print(post['classification'])
-            print(post['data']['score'])
-            print(post['data']['title'])
+    classified_posts = classify_posts(posts, food_type)
 
-    except KeyError as e:
-        print('Error accessing the Reddit API')
-        print(subreddit['message'])
+    for post in classified_posts:
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!')
+        print(food_type)
+        print(post.get('class_score'))
+        print(post.get('data').get('score'))
+        print(post.get('data').get('title'))
+        print(post.get('data').get('selftext'))
 
-display_list(sys.argv[1:][0])
+    print('TOTAL POSTS:')
+    print(len(posts))
+
+    print('TOTAL CLASS POSTS:')
+    print(len(classified_posts))
+    # else:
+    #     print(subreddit.get('message'))
+
+display_list(sys.argv[1:][0], sys.argv[1:][1])
