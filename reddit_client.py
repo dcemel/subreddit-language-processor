4a3d4dac1bc1f6e064567e1ce93f6521a7aa97b8
dc <dcmlcrazy@gmail.com>
2017-09-25 20:48:37 -0400

    Adds exception raising to reddit client

--- reddit_client.py
+++ reddit_client.py
@@ -11,4 +11,5 @@ class RedditClient:
 
         subreddit_url = 'https://www.reddit.com/r/{0}.json?limit=100'.format(self.subreddit)
         response = requests.get(subreddit_url)
+        response.raise_for_status()
         return response.json()
